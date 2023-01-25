78646e2e038bc00b5f6eeeaa74b2579a5167c0a9
mohsin.khan <mohsin.khan@niveussolutions.com>
2023-01-25 17:36:02 +0530

    New Changes

--- src/main/java/com/niveus/CodeAssessor/Controller/CodeAssessorController.java
+++ src/main/java/com/niveus/CodeAssessor/Controller/CodeAssessorController.java
@@ -1,12 +1,14 @@
 package com.niveus.CodeAssessor.Controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.niveus.CodeAssessor.asessor.JavaAssessor;
 import com.niveus.CodeAssessor.model.CodeResult;
 
 @RestController
@@ -16,7 +18,13 @@ public class CodeAssessorController {
 
 		CodeResult codeResult = new CodeResult();
 
-		List<String> results = new ArrayList<String>();
+		List<File> filesArrayList = new ArrayList<File>();
+		List<File> directoryArrayList = new ArrayList<File>();
+		ArrayList<String> excludeArrayList = new ArrayList<String>();
+		excludeArrayList.add("target");
+		excludeArrayList.add(".mvn");
+		excludeArrayList.add(".settings");
+		excludeArrayList.add(".git");
 
 		File[] files = new File(folderPath).listFiles();
 		// If this pathname does not denote a directory, then listFiles() returns null.
@@ -28,19 +36,72 @@ public class CodeAssessorController {
 				String fileName = file.getName();
 
 				if ("pom.xml".equalsIgnoreCase(fileName)) {
-					// It is java based app
-					JavaAssessor javaAccessor = new JavaAssessor(codeResult);
-					javaAccessor.processJavaCode();
+
+					codeResult.setApplicationType("SPRINGBOOT");
+
+				} else if ("package.json".equalsIgnoreCase(fileName)) {
+					codeResult.setApplicationType("NODEJS");
 				}
 
-				results.add(fileName);
+				filesArrayList.add(file);
+			} else if (file.isDirectory()) {
+
+				boolean isExclude = excludeArrayList.contains(file.getName());
+				if (!isExclude) {
+					directoryArrayList.add(file);
+				}
+
 			}
 		}
 
-		// report gen - codeResult be concverted json file get pdf
+		System.out.println("" + filesArrayList);
+		System.out.println("" + directoryArrayList);
 
-		System.out.println("" + results);
-		System.out.println(" Accessing the code......");
+		for (File f : directoryArrayList) {
+			processDirectory(f.getAbsolutePath(), codeResult);
+		}
+
 	}
 
+	public static void processDirectory(String path, CodeResult codeResult) {
+
+		File root = new File(path);
+		File[] list = root.listFiles();
+
+		if (list == null)
+			return;
+
+		for (File f : list) {
+			if (f.isDirectory()) {
+				processDirectory(f.getAbsolutePath(), codeResult);
+				System.out.println("Dir:" + f.getAbsoluteFile());
+			} else {
+
+				if (f.getName().equalsIgnoreCase("application.properties")) {
+					parseApplicationProperties(f.getAbsolutePath());
+				}
+
+				System.out.println("File:" + f.getAbsoluteFile());
+			}
+		}
+
+	}
+
+	public static void parseApplicationProperties(String absolatePath) {
+
+		try {
+			InputStream input = new FileInputStream(absolatePath);
+			Properties prop = new Properties();
+			prop.load(input);
+
+			System.out.println(prop.getProperty("spring.datasource.url"));
+			System.out.println(prop.getProperty("spring.datasource.username"));
+			System.out.println(prop.getProperty("spring.datasource.driver-class-name"));
+			System.out.println(prop.getProperty("spring.jpa.database-platform"));
+
+		} catch (Exception e) {
+
+		}
+
+	}
 }
