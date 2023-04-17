7af3783abf8026bebbf9d5d0e246776a6425484c
mohsin.khan <mohsin.khan@niveussolutions.com>
2023-04-17 16:22:06 +0530

    New chagnes

--- src/main/java/com/niveus/CodeAssessor/Controller/CodeAssessorController.java
+++ src/main/java/com/niveus/CodeAssessor/Controller/CodeAssessorController.java
@@ -1,18 +1,23 @@
 package com.niveus.CodeAssessor.Controller;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.niveus.CodeAssessor.model.CodeResult;
 
 @RestController
 public class CodeAssessorController {
+	
+	int totalNoOfLines = 0;
 
 	public void doCodeAssessment(String folderPath) {
 
@@ -28,6 +33,8 @@ public class CodeAssessorController {
 
 		File[] files = new File(folderPath).listFiles();
 		// If this pathname does not denote a directory, then listFiles() returns null.
+		
+		
 
 		for (File file : files) {
 
@@ -60,10 +67,12 @@ public class CodeAssessorController {
 		for (File f : directoryArrayList) {
 			processDirectory(f.getAbsolutePath(), codeResult);
 		}
+		
+		System.out.println("  totalNoOfLines   >>>>> "+totalNoOfLines);
 
 	}
 
-	public static void processDirectory(String path, CodeResult codeResult) {
+	public  void processDirectory(String path, CodeResult codeResult) {
 
 		File root = new File(path);
 		File[] list = root.listFiles();
@@ -76,6 +85,24 @@ public class CodeAssessorController {
 				processDirectory(f.getAbsolutePath(), codeResult);
 				System.out.println("Dir:" + f.getAbsoluteFile());
 			} else {
+				
+				String ext = FilenameUtils.getExtension(f.getAbsolutePath()); // returns "txt"
+				System.out.println(" ext ---- "+ext);
+				
+				if("java".equalsIgnoreCase(ext)) {
+					try {
+						BufferedReader reader = new BufferedReader(new FileReader(f.getAbsolutePath()));
+						int lines = 0;
+						while (reader.readLine() != null) lines++;
+							reader.close();
+							
+						System.out.println(" lines "+lines);	
+						totalNoOfLines = totalNoOfLines + lines;
+					}
+					catch(Exception e) {
+						e.printStackTrace();
+					}
+				}
 
 				if (f.getName().equalsIgnoreCase("application.properties")) {
 					parseApplicationProperties(f.getAbsolutePath());
